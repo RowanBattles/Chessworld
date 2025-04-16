a014d614cad75e564813dc5e6468a02f217b9937
Rowan <rowanvanderweel@gmail.com>
2025-04-16 10:17:06 +0200

    CORS issues

--- Backend/APIGateway/src/APIGateway.API/Program.cs
+++ Backend/APIGateway/src/APIGateway.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("*")
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
