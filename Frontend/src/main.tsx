92334d3e1619bebf799f965f8a08c7c6a7d3894e
Rowan <rowanvanderweel@gmail.com>
2025-04-17 12:23:57 +0200

    Change base url

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
